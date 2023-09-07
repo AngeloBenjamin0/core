eeb1159132443bb42277ed0c3485bebd9176489a
lucas <lucas.addisi@gmail.com>
2023-09-07 00:59:53 -0300

    Estética

--- src/main/java/org/pp2/Main.java
+++ src/main/java/org/pp2/Main.java
@@ -1,9 +1,7 @@
 package org.pp2;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         String dispositivoJsonConfigPath = args[0];
         String dispositivoId = args[1];
         int temperatura = Integer.parseInt(args[2]);
