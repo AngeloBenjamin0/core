c5740d7a523f5634a9b643cd8f815766f2262b3a
Angelo <angelofaria060@gmail.com>
2023-09-14 22:59:29 -0300

    inicio tests UserStory2

--- src/test/java/us/UserStory1Test.java
+++ src/test/java/us/UserStory1Test.java
@@ -2,11 +2,14 @@ package us;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class UserStory1Test {
 
     @Test
     void testHappyPath(){
+        assertTrue(true);
     }
 
     @Test
---
+++ src/test/java/us/UserStory2Test.java
@@ -0,0 +1,28 @@
+package us;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+/*
+US Extensiblidad: Quiero agregar un climatizador
+ */
+public class UserStory2Test {
+
+    @Test
+    void testDriverClimatizadorInexistente(){
+    }
+
+    @Test
+    void testDriverClimatizadorInvalido(){
+    }
+
+    @Test
+    void testDirectorioVacio(){
+    }
+
+    @Test
+    void testUnicoDriverClimatizador(){
+    }
+    @Test
+    void testMultiplesDriverClimatizadores(){
+    }
+}
