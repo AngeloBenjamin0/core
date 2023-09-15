e0cbbd103c6133c324a9d04f1f0206046d20d31d
Angelo <angelofaria060@gmail.com>
2023-09-15 01:18:49 -0300

    fix tests US2

--- src/test/java/us/UserStory2Test.java
+++ src/test/java/us/UserStory2Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.pp2.DriverClimatizadorFactory;
 import org.pp2.DriverClimatizadorFactoryDiscoverer;
 
+import java.nio.file.FileSystems;
 import java.util.Collections;
 import java.util.Set;
 
@@ -13,29 +14,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 US Extensiblidad: Quiero agregar un climatizador
  */
 public class UserStory2Test {
-    final int EMPTY_SIZE = 0;
-    final static String PATH_VACIO = System.getProperty("user.dir") + "\\src\\test\\resources\\resources_vacio";
-    final static String PATH_SIN_CLIMATIZADOR = System.getProperty("user.dir") + "\\src\\test\\resources";
+    final static int TAMANO_VACIO = 0;
+    final static String PATH_INEXISTENTE = FileSystems.getDefault().getPath("src", "test", "resources", "resources_").toString();
+    final static String PATH_VACIO = FileSystems.getDefault().getPath("src", "test", "resources", "resources_vacio").toString();
+    final static String PATH_CLIMATIZADOR_INVALIDO = FileSystems.getDefault().getPath("src", "test", "resources").toString();
     @Test
-    void testDriverClimatizadorInexistente(){
-        Set<DriverClimatizadorFactory> climatizadorFactorySet = new DriverClimatizadorFactoryDiscoverer().discover(PATH_VACIO);
-        assertEquals(EMPTY_SIZE, climatizadorFactorySet.size());
+    void testPathInexistente(){
+        Set<DriverClimatizadorFactory> climatizadorFactorySet = new DriverClimatizadorFactoryDiscoverer().discover(PATH_INEXISTENTE);
+        assertEquals(TAMANO_VACIO, climatizadorFactorySet.size());
     }
 
     @Test
-    void testDriverClimatizadorInvalido(){
-        Set<DriverClimatizadorFactory> climatizadorFactorySet = new DriverClimatizadorFactoryDiscoverer().discover(PATH_SIN_CLIMATIZADOR);
-        assertEquals(EMPTY_SIZE, climatizadorFactorySet.size());
+    void testDriverClimatizadorInvalido(){ //throws RunTimeException
+        Set<DriverClimatizadorFactory> climatizadorFactorySet = new DriverClimatizadorFactoryDiscoverer().discover(PATH_CLIMATIZADOR_INVALIDO);
+        assertEquals(TAMANO_VACIO, climatizadorFactorySet.size());
     }
 
     @Test
     void testDirectorioVacio(){
+        Set<DriverClimatizadorFactory> climatizadorFactorySet = new DriverClimatizadorFactoryDiscoverer().discover(PATH_VACIO);
+        assertTrue(climatizadorFactorySet.isEmpty());
     }
 
     @Test
     void testUnicoDriverClimatizador(){
+        //assertEquals(1, climatizadorFactorySet.size())
     }
     @Test
     void testMultiplesDriverClimatizadores(){
+        //assertTrue(limatizadorFactorySet.size() > 1)
     }
 }
